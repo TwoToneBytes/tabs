51eb15df1ab6453067a49765f447430ccbbb7efb
msacca <msacca@tinyfactory.co>
2012-07-30 00:22:29 -0700

    added hearts and animation (will change)

--- js/app.js
+++ js/app.js
@@ -4,4 +4,12 @@ $(function() {
    } ,function(){
   		$(this).find('.highlight').children().removeClass('reveal');
 	});
+
+   $('.like').click(function(){
+   	
+   		$(this).append('<div class="heartAni"></div>');
+   		$(this).find('.heartAni').animate({height:"50px", width:"50px", marginTop: "-15px",
+marginLeft: "-13px"}, 100);
+   		$(this).find('.heartAni').fadeTo(150, 0)
+   });
 });
